d3f904fab13bd92b619c8a912ab1096ceca5e941
Rnehasree19 <rnehasree@gmail.com>
2026-05-30 07:13:34 +0530

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 
 # Replace this after deploying FastAPI on Render
-API_URL = "https://your-backend-name.onrender.com/optimize-route"
+API_URL = "https://ada-1-mywf.onrender.com/optimize-route"
 
 st.set_page_config(
     page_title="Wedding Route Optimizer",
